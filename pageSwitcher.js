9ec9e08ee4d472582a22ec3b61ee19f7e555b253
ShadowScripts-coder <122163139+ShadowScripts-coder@users.noreply.github.com>
2024-04-21 17:00:16 +0100

    python course

--- pageSwitcher.js
+++ pageSwitcher.js
@@ -1,8 +1,20 @@
 function changeLang(trg) {
     if (trg == "cy") {
-        location.href = "../cy/index.html";
+        location.href = "../cy/";
+    }
+    if (trg == "cy frm 2") {
+        location.href = "../../cy/";
+    }
+    if (trg == "cy/py") {
+        location.href = "../cy/python";
+    }
+    if (trg == "cy/py/3") {
+        location.href = "../../../cy/python";
+    }
+    if (trg == "cy/py/pygame") {
+        location.href = "../../cy/python/pygame";
     }
     if (trg == "en") {
-        location.href = "../en/index.html";
+        location.href = "../en/";
     }
 }
